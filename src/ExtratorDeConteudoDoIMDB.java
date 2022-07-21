78c0e29c8773bd202949cd317527718e5d31346a
Wilson Santos <contato@wilsonsantos.net>
2022-07-21 11:49:59 -0300

    ✨ Add extrator para o IMDB

---
+++ src/ExtratorDeConteudoDoIMDB.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import Interfaces.IExtratorConteudo;
+import Model.Conteudo;
+
+public class ExtratorDeConteudoDoIMDB implements IExtratorConteudo {
+
+  @Override
+  public List<Conteudo> extraiConteudos(String json) {
+    var parser = new JsonParser();
+    List<Map<String,String>> listaDeAtributos = parser.parse(json);
+    List<Conteudo> conteudos = new ArrayList<>();
+
+    //popula a lista
+    for (Map<String, String> atributos : listaDeAtributos) {
+      String titulo = atributos.get("title");
+      String urlImage = atributos.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
+
+      Conteudo conteudo = new Conteudo(titulo, urlImage);
+      conteudos.add(conteudo);
+    }
+    return conteudos;
+  }
+  
+}
